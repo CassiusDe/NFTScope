d6349b511c7f2ff6769bc619ca0c0ed66faaa0c6
CassiusDe <ouzacpj605@hotmail.com>
2025-02-14 19:42:17 +0800

    add basic api server
    created express server with health endpoint
    added env config template

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'NFTScope API',
+    version: '0.1.0',
+    status: 'running'
+  });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`NFTScope server running on port ${PORT}`);
+});
